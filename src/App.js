3eb6a9aace6c52299267fa7440e50381281baf1e
Zachary Taylor <Ztaylo273@west-mec.local>
2021-05-06 15:23:27 -0700

    tried adding the nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <img className="logo" src="#"/>
+    <nav className="navbar navbar-inverse">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <a className="navbar-brand" href="#">WebSiteName</a>
     </div>
+    <ul className="nav navbar-nav">
+      <li className="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+    <div className="toDayInfo">
+
+    </div>
+    <div className="hourly">
+
+    </div>
+    <div className="tomorrowInfo">
+
+    </div>
+    <div className="ads">
+
+    </div>
+    <footer>
+
+    </footer>
+    </>
   );
 }
 
