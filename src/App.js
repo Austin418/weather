83ad4a4a3e8abc257e58db30954ee83f0fc6e7db
Zachary Taylor <Ztaylo273@west-mec.local>
2021-05-07 15:00:47 -0700

    Added nav bar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import './App.css';
+import Smallnav from './components/Smallnav'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// import {Nav, Navbar, Form, FormControl, Button} from react-bootstrap
 
 function App() {
   return (
     <>
-    <img className="logo" src="#"/>
-    <nav className="navbar navbar-inverse">
-  <div className="container-fluid">
-    <div className="navbar-header">
-      <a className="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul className="nav navbar-nav">
-      <li className="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
-    </ul>
-  </div>
-</nav>
-    <div className="toDayInfo">
-
-    </div>
-    <div className="hourly">
-
-    </div>
-    <div className="tomorrowInfo">
-
-    </div>
-    <div className="ads">
-
-    </div>
-    <footer>
-
-    </footer>
+    <Smallnav />
     </>
   );
 }
