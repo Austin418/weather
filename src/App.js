a0d7b95d66793eb6632789bba98e9b5293b35e39
Zachary Taylor <Ztaylo273@west-mec.local>
2021-05-07 15:21:17 -0700

    Added JSON

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Smallnav from './components/Smallnav'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import HeaderNav from './components/HeaderNav'
+import data from './info/weather.json'
 
 
 function App() {
